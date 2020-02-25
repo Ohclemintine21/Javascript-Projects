dd5d30f4204537d7b1e1026fd443945893834a24
Ohclemintine21 <60559963+Ohclemintine21@users.noreply.github.com>
2020-02-25 14:49:32 -0800

    Variables, strings, concatenate elements
    assignments

---
+++ Basic Javascript Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,25 @@
+window.alert("Hey,Hey!");
+document.write("Hello everyone!");
+var A="How are you?";
+document.write(A);
+
+var B= "How much wood,"
++"could a wood chuck chuck?\""
++"if a wood chuck could chuck wood?";
+document.write(B);
+var family="The Jones", Dad="Ted", Mom="Elise",
+    Daughter="Allie", Son="Sebastian";
+document.write(Mom,Daughter,Dad,Son);
+var daughter="Allie is the daughter.";
+var daughter=daughter.fontcolor("purple");
+document.write(daughter);
+var Mom="Elise is the Mom.";
+var Mom=Mom.fontcolor("green");
+document.write(Mom);
+var dad="Ted is the dad.";
+var dad=dad.fontcolor("blue");
+document.write(dad);
+var son="Sebastian is the son.";
+var son=son.fontcolor("orange");
+document.write(son);
+document.write(6+6);

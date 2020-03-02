2643a6e7bc90346685f89f7a78c879b79f1b1a36
Stephanie Schulz <ohclemintine21@gmail.com>
2020-03-01 17:49:47 -0800

    continue and break examples

--- Basic Javascript Projects/JS/Basic_Javascript_10.js
+++ Basic Javascript Projects/JS/Basic_Javascript_10.js
@@ -22,12 +22,55 @@ function for_Loop() {
     }
     document.getElementById("List_of_Instruments").innerHTML=Content;
 } 
-function cat_pics() {
-    var Cat_Picture=[];
-    Cat_Picture[0]= "sleeping";
-    Cat_Picture[1]= "playing";
-    Cat_Picture[2]= "eating";
-    Cat_Picture[3]= "purring";
-    document.getElementById("Cat").innerHTML="In this picture, the cat is " +
-        Cat_Picture[2] + ".";
+function array_Function() {
+    var fox_Picture=[];
+    fox_Picture[0]= "sleeping";
+    fox_Picture[1]= "playing";
+    fox_Picture[2]= "eating";
+    fox_Picture[3]= "purring";
+    document.getElementById("Array").innerHTML="In this picture, the fox is " +
+        fox_Picture[2] + ".";
 }
+function constant_function() {
+    const Used_Car= {type:"truck", make:"Nissan", model:"Titan"};
+    Used_Car.color= "green";
+    Used_Car.price= "$2,500";
+    document.getElementById("Constant").innerHTML="The cost of the " +
+        Used_Car.type + " was " + Used_Car.price;
+}
+
+var X = 82; //This is a declaration that assigns X the value 82
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
+
+function truck_Object() {
+    let truck = {
+    make: "Nissan ",
+    model: "Titan ",
+    year: "2019 ",
+    color: "green ",
+    description : function() {
+        return "The truck is a " + this.year + this.color + this.model;
+    }
+};
+document.getElementById("truck").innerHTML= truck.description();
+}
+
+
+    var text = "";
+    var i;
+for (i = 0; i < 10; i++) {
+    if (i == 3) { break; }
+    text += " The number is " + i + "<br>";
+    }
+document.getElementById("Test").innerHTML= text;
+
+for (i = 0; i < 10; i ++) {
+    if (i === 3) { continue; }
+    text += "The number is " + i + "<br>";
+}
+document.getElementById("Demo").innerHTML = text; 
